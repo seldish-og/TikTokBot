15d3e24527d2d6830023f100000e2e12a93ce347
seldish-og <82537848+seldish-og@users.noreply.github.com>
2021-06-18 12:55:29 -0400

    add login method

--- tik_tok.py
+++ tik_tok.py
@@ -4,12 +4,18 @@ from selenium import webdriver
 
 from tik_tok_auth import username, password
 
+from selenium.webdriver.common.keys import Keys
+
 
 class TikTokBot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.browser = webdriver.Chrome("chromedriver.exe")
+
+    def close_browser(self):
+        self.browser.close()
+        self.browser.quite()
 
     def add_discription(self):
         list_of_hashtags = ['#meme', '#tranding', '#trand', '#recommendations', '#tranding', '#trand', 
@@ -20,6 +26,29 @@ class TikTokBot:
         text_description = []
         # return 
 
+    def login(self):
+        try:
+            self.browser.get('https://www.tiktok.com/login/phone-or-email/email')
+            time.sleep(random.randrange(2, 4))
+            # find the field username
+            user_input = self.browser.find_element_by_name('email') 
+            # clear the field (just in case)
+            user_input.clear()
+            # write the username
+            user_input.send_keys(username)  
+
+            time.sleep(random.randrange(2, 4))
+            # find the field username
+            password_input = self.browser.find_element_by_name('password')  
+            password_input.clear()
+            # write the password
+            password_input.send_keys(password)  
+            # send the complete authorization form
+            password_input.send_keys(Keys.ENTER)  
+
+        except Exeption as ex:
+        print(ex)
+        self.close_browser()
 
 tik_tok_parser = TikTokBot(username, password)
 tik_tok_parser.add_discription()
