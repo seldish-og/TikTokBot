b291d6b0da5987b311f98f75802a49891cfc995a
seldish-og <82537848+seldish-og@users.noreply.github.com>
2021-06-19 12:44:17 +0300

    Update tik_tok.py

--- tik_tok.py
+++ tik_tok.py
@@ -1,10 +1,10 @@
 import random
+import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from tik_tok_auth import username, password
-
-from selenium.webdriver.common.keys import Keys
 
 
 class TikTokBot:
@@ -15,40 +15,41 @@ class TikTokBot:
 
     def close_browser(self):
         self.browser.close()
-        self.browser.quite()
+        self.browser.quit()
 
     def add_discription(self):
-        list_of_hashtags = ['#meme', '#tranding', '#trand', '#recommendations', '#tranding', '#trand', 
+        list_of_hashtags = ['#meme', '#tranding', '#trand', '#recommendations', '#tranding', '#trand',
                             '#recommendations', '#wow', '#batman', '#hightechnologies']
         random_quantity = random.randrange(2, 4)
-        hashtags = random.sample(list_of_hashtags, random_quantity) # return random unique hashtags from the list
+        hashtags = random.sample(list_of_hashtags, random_quantity)  # return random unique hashtags from the list
         smiles = []
         text_description = []
-        # return 
+        # return
 
     def login(self):
         try:
             self.browser.get('https://www.tiktok.com/login/phone-or-email/email')
             time.sleep(random.randrange(2, 4))
             # find the field username
-            user_input = self.browser.find_element_by_name('email') 
+            user_input = self.browser.find_element_by_name('email')
             # clear the field (just in case)
             user_input.clear()
             # write the username
-            user_input.send_keys(username)  
+            user_input.send_keys(username)
 
             time.sleep(random.randrange(2, 4))
             # find the field username
-            password_input = self.browser.find_element_by_name('password')  
+            password_input = self.browser.find_element_by_name('password')
             password_input.clear()
             # write the password
-            password_input.send_keys(password)  
+            password_input.send_keys(password)
             # send the complete authorization form
-            password_input.send_keys(Keys.ENTER)  
+            password_input.send_keys(Keys.ENTER)
 
-        except Exeption as ex:
-        print(ex)
+        except Exception as ex:
+            print(ex)
         self.close_browser()
+
 
 tik_tok_parser = TikTokBot(username, password)
 tik_tok_parser.add_discription()
