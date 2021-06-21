20ecabd91d95fb8e85eb6b71e209394c2434d5a2
seldish-og <82537848+seldish-og@users.noreply.github.com>
2021-06-21 17:05:38 +0300

    save captcha pics after getting to the site

--- tik_tok.py
+++ tik_tok.py
@@ -1,7 +1,9 @@
 import random
 import time
 
+import requests
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
 from tik_tok_auth import email, password
@@ -26,23 +28,50 @@ class TikTokBot:
         text_description = []
         # return
 
+    def xpath_exists(self, xpath):
+
+        browser = self.browser
+        try:
+            browser.find_element_by_xpath(xpath)
+            exist = True
+        except NoSuchElementException:
+            exist = False
+        return exist
+
     def login(self):
         try:
-            self.browser.get('https://www.tiktok.com') # '...tiktok.com/login' doesnt work, ask to download an app 
-            time.sleep(1)
+            self.browser.get('https://www.tiktok.com')
+            time.sleep(random.randrange(1, 3))
+
             # there must be captcha solution and a loop to solve it several times
             # or login manually and use cookies next times
+
+            # find captcha on the page
+            try:  # get captcha and captcha key to match them 
+                captcha_xpath = "/html/body/div[6]/div"
+                if self.xpath_exists(captcha_xpath):
+                    captcha_src_url = self.browser.find_element_by_xpath(
+                        '/html/body/div[6]/div/div[2]/img[1]').get_attribute("src")
+                    captcha_key_src_url = self.browser.find_element_by_xpath(
+                        '/html/body/div[6]/div/div[2]/img[2]').get_attribute("src")
+                    captcha = requests.get(captcha_src_url, stream=True)
+                    captcha_key = requests.get(captcha_key_src_url, stream=True)
+            except Exception as ex:
+                print(ex)
+
             # find and click log in button
             self.browser.find_element_by_class_name('login-button').click()
             time.sleep(20)
+
             # switch to iframe
             # iframe = self.browser.find_element_by_xpath('/html/body/div[2]/div[1]/iframe')
             # self.browser.switch_to.frame(iframe)
+
             # open auth by email
             self.browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/div[2]').click()
+
             # find username input
             user_input = self.browser.find_element_by_name('email')
-            # clear the field (just in case)
             user_input.clear()
             # write the username
             user_input.send_keys(email)
